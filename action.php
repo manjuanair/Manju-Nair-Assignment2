cb37dcb6158b2f7b49897c0fac58dffe11cda8c5
Manju Ananthakrishnan Nair <manjuanair2021@gmail.com>
2023-04-16 22:18:33 -0300

    Redone the assignment
    Redone the assignment

---
+++ action.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Welcome <?php echo $_GET["title"]; ?><?php echo $_GET["fname"]; ?><?php echo $_GET["lname"]; ?><br>
+Your role is: <?php echo $_GET["role"]; ?>
+</body>
+</html>
--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Halifax Canoe and Kayak</p>
-</footer>
-
-</body>
-</html>
